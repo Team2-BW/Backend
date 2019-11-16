07379b2275874f986806739b9d97c983fbe7e8d4
ltann <leeroyleeroy18@gmail.com>
2019-11-15 23:13:20 -0800

    Able to manually traverse map

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import config
 import requests
+import time
+from utils import Stack, Queue
+from room import Room
 
 token = config.TOKEN
 
 headers = {
-    'Authorization': f"TOKEN {token}",
+    'Authorization': f"Token {token}",
     'Content-Type': 'application/json'
 }
 
@@ -12,4 +15,34 @@ init_response = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv
 data = init_response.json()
 print(data)
 
-print(data['room_id'], data['title'], data['coordinates'], data['exits'], data['cooldown'])
+time.sleep(data['cooldown'])
+
+roomID = data['room_id']
+room_exits = data['exits']
+
+reverseDirections = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+traversalPath = []
+reversePath = []
+visitedDict = {}
+MapRoom = []
+
+# Start with current room 0 and get it's exits
+visitedDict[roomID] = room_exits
+
+# Traverse entire graph while the rooms visited is less than 500
+while len(MapRoom) < 500:
+
+    post_data = {
+        "direction": input("Enter your direction: ")
+    }
+    
+    # Request to move
+    res = requests.post("https://lambda-treasure-hunt.herokuapp.com/api/adv//move", json=post_data, headers=headers)
+    data = res.json()
+    roomID = data['room_id']
+
+    print(data)
+    MapRoom.append(roomID)
+    print(MapRoom)
+
+    time.sleep(data['cooldown'])
