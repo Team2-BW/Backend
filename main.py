3b589868385d694588368a21ab4c9292e7ce0a48
ltann <leeroyleeroy18@gmail.com>
2019-11-18 00:50:58 -0800

    Able to pick up items

--- main.py
+++ main.py
@@ -16,33 +16,51 @@ data = init_response.json()
 print(data)
 
 time.sleep(data['cooldown'])
-
-roomID = data['room_id']
 room_exits = data['exits']
+roomID = data['room_id']
+
+roomInfo = f'room_id: {data["room_id"]}, title: {data["title"]}, coords: {data["coordinates"]}'
+print(roomInfo)
 
 reverseDirections = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 traversalPath = []
 reversePath = []
-visitedDict = {}
+visitedRoom = {}
 MapRoom = []
 
 # Start with current room 0 and get it's exits
-visitedDict[roomID] = room_exits
+# visitedRoom[roomID] = room_exits
 
 # Traverse entire graph while the rooms visited is less than 500
-while len(MapRoom) < 500:
+while len(visitedRoom) < 500:
+    unvisited = []
+    room_exits = data['exits']
+    items = data['items']
+    
+    # take the items in room
+    if len(items) > 0:
+        for item in items:
+            res = requests.post("https://lambda-treasure-hunt.herokuapp.com/api/adv/take/", json={'name': f'{item}'}, headers=headers)
+            print(res)
+            time.sleep(data['cooldown'])
+    
+    # check stats
+    res = requests.post("https://lambda-treasure-hunt.herokuapp.com/api/adv/status/", headers=headers)
+    stats = res.json()
+    print(stats)
+    time.sleep(data['cooldown'])
+    
+    visitedRoom[data["room_id"]] = data["coordinates"]
+    print("visited room", visitedRoom)
 
+    # enter next direction
     post_data = {
         "direction": input("Enter your direction: ")
     }
     
-    # Request to move
     res = requests.post("https://lambda-treasure-hunt.herokuapp.com/api/adv//move", json=post_data, headers=headers)
     data = res.json()
     roomID = data['room_id']
 
     print(data)
-    MapRoom.append(roomID)
-    print(MapRoom)
-
     time.sleep(data['cooldown'])
